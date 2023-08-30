4785eca229ba768107c7603622e9a6a60d59f794
talhagozutok <talhagztk@gmail.com>
2023-08-30 18:49:03 +0300

    Add project files.

---
+++ UrlShortener/Extensions/MigrationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        if (dbContext.Database.GetPendingMigrations().Any())
+        {
+            dbContext.Database.Migrate();
+        }
+    }
+}
