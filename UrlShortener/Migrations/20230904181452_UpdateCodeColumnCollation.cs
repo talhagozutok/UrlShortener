b7494258469453b8592043d938fbe2257f3e32c8
talhagozutok <talhagozutok@users.noreply.github.com>
2023-09-04 21:18:30 +0300

    Updated `Code` column * Collation for case-sensitive

---
+++ UrlShortener/Migrations/20230904181452_UpdateCodeColumnCollation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCodeColumnCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "ShortenedUrls",
+                type: "nvarchar(7)",
+                maxLength: 7,
+                nullable: false,
+                collation: "SQL_Latin1_General_CP1_CS_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(7)",
+                oldMaxLength: 7);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "ShortenedUrls",
+                type: "nvarchar(7)",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(7)",
+                oldMaxLength: 7,
+                oldCollation: "SQL_Latin1_General_CP1_CS_AS");
+        }
+    }
+}
