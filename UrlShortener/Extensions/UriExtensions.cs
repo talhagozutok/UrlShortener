dad27c63ac0000931b66861e20f3f6051bb81899
talhagozutok <talhagozutok@users.noreply.github.com>
2023-09-04 21:33:32 +0300

    Refactor

---
+++ UrlShortener/Extensions/UriExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Extensions;
+
+public static class UriExtensions
+{
+    public static bool IsValidUrl(string url)
+        => Uri.TryCreate(url, UriKind.Absolute, result: out _);
+}
